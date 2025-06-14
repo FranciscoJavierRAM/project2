53b503e8fbb7bcba71231cf23dbe2cf89805a85e
FranciscoJavierRAM <pacorro-2014@hotmail.com>
2025-06-13 23:02:15 -0400

    Add files via upload

--- priority_list.py
+++ priority_list.py
@@ -4,6 +4,40 @@
 # (2) prioritizing the list,
 # (3) deleting the entire list at once, and
 # (4) marking the current "most want to do" item as done
+#SDM this is my attempt at creating an auto focus app. I'll start the program by creating the
+#  menu. Each menu option will a number the user can type to access that option. Depending on
+#  the number typed the program called upon a specific function. For the add a new item function
+#  the function will ask for an input and save it as the variable 'answer'. Then it will check that
+#  the answer is not empty text. If it is, it gets rejected and you get sent back to the main menu.
+#  If the input is valid it gets saved as a list that includes the answer and its marker. We will attempt
+#  to use nested lists to solve the algorithm.
+# For the delete function we will use the same method as when we created our todo list in class. It asks
+#  for the user to input the specific number of the list item they would like to remove. It checks if the
+#  answer is valid b checking if its a number and if the number is within the list items. If the user types all
+# the entire list gets deleted.
+# Now comes the function that I never got to work properly. This function was suppose to look for the first
+#  blank/new task in the list and mark it as prioritized(o). It then would go to the next item in the list with a
+#  blank and ask if you would like to prioritize that item over the last one. if you said yes then those items were
+#  marked with a "o" until it went over the entire list. After that process was completed it would use the nested lists
+#  to and the marks to look for the priotized tasks and put them in a new list. At the same time it would create a
+#  second list where all the blank tasks would be placed. Then theyy would be combined in order to move all the
+#  prioritized tasks to the end of the new list. I tried to creat a bubbling to to the end effect through this
+#  function. I used a copy of the original list to do so because otherwise it would now work as intended.
+# The highest priority done function was surpeisingly the easiest.I found online that if you add tripple negative 1s
+#  the behaviour of the "for" loop gets reversed. It starts by looking for the last item of the list and looks at its
+#  marker if its a "o" then it gets assigned an "x" to be marked as complete. In case it goes over the list and doest find
+# any priotized tasks it will print the message "No prioritized tasks were found"
+# The display function is also pretty straight forward it uses a for loop to make sure it prints every item on the list.
+# It then prints formated string with the index # + 1 to not display 0, the task from[i][0] and its marker[i][1].
+# I didn't create a function for the last option quit. What I did is I created a boolean for listing and while loop for the
+#  program to run as long as listing was true. If the user pressed 6 it will make listing False thus stopping the 
+# program.
+#  Overall this program was very challenging as well as very "fun". In the way that most of the things I tried dind't
+#  work but when I got something to work as I invisioned, it was very gratifying. I spent at least 10 hours working on this and
+#  I was able to use many of the techniques we learned in class as well as new ones I found online. The reason I tried using
+#  nested lists was because that was the latest zybooks topic I had worked on and I had the material fresh in my head and it
+#  seemed like the best option at the time. I Wish I was able to work on this project during different circumstances as I leave with a sour
+# taste in my mouth because I was never able to get it to work 100% the way I wanted to. 
 awesome_list = []
 
 print("Welcome to my Auto-Focus App")
